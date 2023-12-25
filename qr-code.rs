7f0d438eb6d01851f794816b847b98be0ea10f7b
Grey <70687294+B0bbycode@users.noreply.github.com>
2023-12-25 22:46:36 +0200

    Create qr-code.rs

---
+++ qr-code.rs
@@ -0,0 +1,33 @@
+extern crate qrcode;
+extern crate image;
+
+use qrcode::QrCode;
+use qrcode::render::svg;
+use image::{GenericImageView, DynamicImage, Rgba};
+
+fn main() {
+    // Your data for the QR code
+    let data = "Hello, QR Code with Image!";
+
+    // Generate QR code
+    let code = QrCode::new(data).unwrap();
+    let image = code.render::<svg::Color>().build();
+
+    // Load image to be placed in the center
+    let center_image_path = "path/to/your/image.png";
+    let center_image = image::open(center_image_path).unwrap();
+
+    // Calculate the position to center the image on the QR code
+    let x_offset = (image.width() - center_image.width()) / 2;
+    let y_offset = (image.height() - center_image.height()) / 2;
+
+    // Combine QR code and image
+    let mut final_image = DynamicImage::new_rgba8(image.width(), image.height());
+    final_image.copy_from(&image, 0, 0).unwrap();
+    final_image.copy_from(&center_image, x_offset as u32, y_offset as u32).unwrap();
+
+    // Save the final image
+    let output_path = "path/to/your/output/image.png";
+    final_image.save(output_path).unwrap();
+    println!("QR code with center image saved to: {}", output_path);
+}
